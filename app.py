9415841734df1151da73c9ecf079c918dbde32e5
Kleyton Iarlynson <klei-iarli@hotmail.com>
2023-03-15 12:58:53 -0300

    Tentativa 1

--- app.py
+++ app.py
@@ -15,8 +15,13 @@ def hello() -> str:
 
 
 @app.route('/calcular_raiz', methods=['POST'])
-def raiz() -> str:
-    ...
+def pag_raiz() -> html:
+    a_raiz = request.form['a_raiz']
+    return render_template(
+    'raiz.html',
+    the_title = 'Calcule o valor da raiz'
+
+    )
 
 
 @app.route('/busca_palavra', methods=['POST'])
@@ -30,7 +35,7 @@ def do_search() -> str:
         the_title=title,
         the_results=results,
         a_frase=frase,
-        as_letras=letras,
+        as_letras=letras
     )
 
 
