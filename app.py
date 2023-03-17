2357134a52c577da8f3ef2758263df1a14e41690
Kleyton Iarlynson <klei-iarli@hotmail.com>
2023-03-16 22:16:38 -0300

    Corrigindo aplicação

--- app.py
+++ app.py
@@ -14,15 +14,30 @@ def hello() -> str:
     return "Ola Mundo do Flask"
 
 
-@app.route('/calcular_raiz', methods=['POST'])
+@app.route('/calcular_raiz')#-- , methods=['POST'])
 def pag_raiz() -> html:
-    a_raiz = request.form['a_raiz']
+   # a_raiz = request.form['a_raiz']
     return render_template(
     'raiz.html',
     the_title = 'Calcule o valor da raiz'
-
     )
 
+@app.route('/resultado_raiz', methods=['POST'])
+def projetar() -> str:
+    resultado = request.form['a_raiz']
+    print(resultado)
+    x1 = int(resultado[1:2])
+    x2 = int(resultado[3:4])
+    y1 = int(resultado[7:8])
+    y2 = int(resultado[9:10])
+    p1 = (x1, x2)
+    p2 = (y1, y2)
+    resultado = alg(p1, p2)
+    results = 'Resultado do calculo'
+    return render_template(
+        'raiz2.html',
+        
+    )
 
 @app.route('/busca_palavra', methods=['POST'])
 def do_search() -> str:
